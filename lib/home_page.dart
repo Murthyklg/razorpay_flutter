a33824f5c6b692206cf3e44f0f0ad3cc001801a9
Murthyklg <klgmurthy4797@gmail.com>
2023-10-19 19:43:23 +0530

    first commit of razorpay flutter part

---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // Initializing code here
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Disposing code here
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // UI code here
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
